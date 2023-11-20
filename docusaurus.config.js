d243d657f79e090b1f0251bc6974232024933353
Tran Ngoc Tuan Anh <anhtnt@elightup.com>
2023-11-20 10:20:14 +0700

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -68,7 +68,7 @@ const config = {
       },
       image: 'https://i.imgur.com/wnJtgSC.jpg',
       announcementBar: {
-        content: '<b>🔥 Black Friday Deal 🔥:</b> Up to <b>30% OFF</b> until <b>Dec 30</b>. <a target="_blank" rel="noopener noreferrer" href="https://metabox.io/pricing/">Get it now!</a>',
+        content: '<b>🔥 Black Friday Deal 🔥:</b> Up to <b>30% OFF</b> until <b>Nov 30</b>. <a target="_blank" rel="noopener noreferrer" href="https://metabox.io/pricing/">Get it now!</a>',
         backgroundColor: '#fed7aa',
         textColor: '#7c2d12',
         isCloseable: false,
