6536956efc05616a2507b081cf5630551c03d375
Anh Tran <anhtnt@elightup.com>
2024-11-25 06:46:41 +0700

    Add Black Friday 2024 banner

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -62,14 +62,12 @@ const config = {
         respectPrefersColorScheme: true,
       },
       image: 'https://i.imgur.com/wnJtgSC.jpg',
-      /*
       announcementBar: {
-        content: '<b>🔥 Black Friday Deal 🔥:</b> Up to <b>30% OFF</b> until <b>Nov 30</b>. <a target="_blank" rel="noopener noreferrer" href="https://metabox.io/pricing/">Get it now!</a>',
+        content: '<b>🔥 Black Friday Deal 🔥:</b> <b>20% OFF</b> until <b>Dec 04</b>. <a target="_blank" href="https://metabox.io/pricing/">Get it now!</a>',
         backgroundColor: '#fed7aa',
         textColor: '#7c2d12',
         isCloseable: false,
       },
-      */
       navbar: {
         hideOnScroll: true,
         logo: {
