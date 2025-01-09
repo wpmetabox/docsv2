ee5692de0038448ef64d9df2069ac68ea3da9bbf
Anh Tran <anhtnt@elightup.com>
2025-01-09 17:44:12 +0700

    Add notification bar

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -62,14 +62,12 @@ const config = {
         respectPrefersColorScheme: true,
       },
       image: 'https://i.imgur.com/wnJtgSC.jpg',
-      /*
       announcementBar: {
-        content: '<b>🔥 Black Friday Deal 🔥:</b> <b>20% OFF</b> until <b>Dec 04</b>. <a target="_blank" href="https://metabox.io/pricing/">Get it now!</a>',
+        content: 'Lauching <b>Meta Box Lite</b>, a feature-rich free UI version of Meta Box. <a target="_blank" href="https://metabox.io/lite/">Learn more</a>',
         backgroundColor: '#fed7aa',
         textColor: '#7c2d12',
         isCloseable: false,
       },
-      */
       navbar: {
         hideOnScroll: true,
         logo: {
